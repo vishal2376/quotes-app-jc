aacf4279c58cea417926983a13a9588f280216e5
vishal2376 <vishalsingh2376@gmail.com>
2023-08-28 14:36:42 +0530

    init

---
+++ app/src/main/java/com/vishal2376/jcquoteapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.vishal2376.jcquoteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vishal2376.jcquoteapp.ui.theme.JCQuoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/vishal2376/jcquoteapp/screens/QuoteListItem.kt
@@ -0,0 +1,8 @@
+package com.vishal2376.jcquoteapp.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun QuoteListItem() {
+
+}
