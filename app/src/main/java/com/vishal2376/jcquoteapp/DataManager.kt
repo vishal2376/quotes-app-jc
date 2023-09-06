643f0675ebba5ed0c3c26c2ca437974937f03328
Vishal Singh <vishalsingh2376@gmail.com>
2023-09-06 15:21:57 +0530

    update: add screens and data

--- app/src/main/java/com/vishal2376/jcquoteapp/DataManager.kt
+++ app/src/main/java/com/vishal2376/jcquoteapp/DataManager.kt
@@ -1,17 +1,20 @@
 package com.vishal2376.jcquoteapp
 
 import android.content.Context
+import androidx.compose.runtime.mutableStateOf
 import com.google.gson.Gson
 import com.vishal2376.jcquoteapp.models.Quote
 
 object DataManager {
 
     var data = emptyArray<Quote>()
+    var isDataLoaded = mutableStateOf(false)
 
     fun loadAssetsFromFile(context: Context) {
         context.assets.open("quotes.json").use { inputStream ->
             val json = inputStream.bufferedReader().use { it.readText() }
             data = Gson().fromJson(json, Array<Quote>::class.java)
+            isDataLoaded.value = true
         }
     }
 
--- app/src/main/java/com/vishal2376/jcquoteapp/MainActivity.kt
+++ app/src/main/java/com/vishal2376/jcquoteapp/MainActivity.kt
@@ -3,25 +3,45 @@ package com.vishal2376.jcquoteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.vishal2376.jcquoteapp.ui.theme.JCQuoteAppTheme
+import androidx.compose.ui.unit.sp
+import com.vishal2376.jcquoteapp.screens.QuoteListScreen
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //load json data
+        CoroutineScope(Dispatchers.IO).launch {
+            DataManager.loadAssetsFromFile(applicationContext)
+        }
+
         setContent {
             App()
         }
     }
+}
 
-    @Composable
-    fun App() {
+@Composable
+fun App() {
+    if (DataManager.isDataLoaded.value) {
+        QuoteListScreen(data = DataManager.data) {
 
+        }
+    } else {
+        Box(
+            contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize(1f)
+        ) {
+            Text(text = "Loading...", fontSize = 20.sp)
+        }
     }
 }
+
