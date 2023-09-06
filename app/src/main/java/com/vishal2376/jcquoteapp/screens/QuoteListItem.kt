e8c20c5542ef156fc1f3e9791ed6082875605940
Vishal Singh <vishalsingh2376@gmail.com>
2023-09-06 14:21:36 +0530

    update:add quote list + quote detail UI

--- app/src/main/java/com/vishal2376/jcquoteapp/screens/QuoteListItem.kt
+++ app/src/main/java/com/vishal2376/jcquoteapp/screens/QuoteListItem.kt
@@ -1,8 +1,119 @@
 package com.vishal2376.jcquoteapp.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.FormatQuote
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.util.Collections.rotate
 
 @Composable
 fun QuoteListItem() {
+    Card(elevation = CardDefaults.cardElevation(5.dp), shape = RoundedCornerShape(10.dp)) {
+        Row(modifier = Modifier.padding(16.dp)) {
+            Image(
+                imageVector = Icons.Filled.FormatQuote,
+                colorFilter = ColorFilter.tint(Color.White),
+                contentDescription = null,
+                alignment = Alignment.TopStart,
+                modifier = Modifier
+                    .size(40.dp)
+                    .rotate(180f)
+                    .background(Color.Black)
+            )
+            Spacer(modifier = Modifier.padding(4.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = "Time is Money",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 8.dp)
+                )
 
+                Box(
+                    modifier = Modifier
+                        .background(Color.Gray)
+                        .fillMaxWidth(.4f)
+                        .height(1.dp)
+                )
+
+                Text(
+                    text = "Time is Money",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Thin,
+                    modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 8.dp)
+                )
+
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun QuoteDetail() {
+    Box(
+        contentAlignment = Alignment.Center, modifier = Modifier
+            .fillMaxSize(1f)
+            .background(
+                Brush.sweepGradient(
+                    colors = listOf(
+                        Color(0xFFffffff), Color(0xFFe3e3e3)
+                    )
+                )
+            )
+    ) {
+        Card(
+            elevation = CardDefaults.cardElevation(5.dp),
+            shape = RoundedCornerShape(4.dp),
+            colors = CardDefaults.cardColors(Color.White),
+            modifier = Modifier
+                .padding(32.dp)
+                .fillMaxWidth(1f)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center, modifier = Modifier.padding(16.dp, 24.dp)
+            ) {
+                Image(
+                    imageVector = Icons.Filled.FormatQuote,
+                    modifier = Modifier
+                        .size(80.dp)
+                        .rotate(180f),
+                    contentDescription = null
+                )
+                Text(
+                    text = "Time is money", style = MaterialTheme.typography.headlineMedium
+                )
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = "Time is money", style = MaterialTheme.typography.bodyLarge
+                )
+            }
+        }
+    }
 }
